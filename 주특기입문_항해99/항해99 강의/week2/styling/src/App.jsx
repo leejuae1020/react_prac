441bf6236281056796ae7e8e46439275e53cd5e8
leejuae1020 <leejuae1020@naver.com>
2023-02-10 15:25:59 +0900

    week2 useState 공부

---
+++ 주특기입문_항해99/항해99 강의/week2/styling/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <span>Default Style 테스트</span>
+      <h1>나는 h1 태그</h1>
+      <p>나는야 P태그</p>
+    </div>
+  );
+}
+
+export default App;
